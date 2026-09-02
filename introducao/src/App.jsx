49ce89af420c0e6720abadbbbd374fd97d2dc068
Protti <rafaelprotti2@gmail.com>
2026-09-02 17:14:02 -0300

    setState

--- introducao/src/App.jsx
+++ introducao/src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
 function App() {
+
+  let nome = "Protttttttttttti"
+  let sobrenome = "Rafael"
+  let nome_completo = nome + " " + sobrenome
+  const [cliques, setCliques] = useState(0) //useState
+
+  function contarClique(){
+    setCliques(cliques + 1)
+  }
 
   return (
     <div>
       <h1>Olá Mundo</h1>
       <p>Estou aprendendo React!!!!!! :P</p>
+      <hr/>
+      <p>Meu nome é {nome_completo}</p>
+      <hr/>
+
+      <p>Você clicou {cliques} vezes</p>
+      <button onClick={contarClique}>Clique aqui</button>
+
     </div>
   )
 }
