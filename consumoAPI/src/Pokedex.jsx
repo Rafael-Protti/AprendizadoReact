83f8bb47f1266ca5985fb573a6633bbe0d31dbee
Protti <rafaelprotti2@gmail.com>
2026-09-11 16:54:47 -0300

    Atividade funcionando

--- consumoAPI/src/Pokedex.jsx
+++ consumoAPI/src/Pokedex.jsx
@@ -16,6 +16,8 @@ function Pokedex() {
         buscarPokemon(pesquisa)
     }, [])
 
+    console.log(pokemon.sprites?.other["official-artwork"].front_default)
+
     return ( 
         <div>
             <h1>Pokédex</h1>
@@ -23,13 +25,12 @@ function Pokedex() {
 
             <input onChange={e => setPesquisa(e.target.value)} type="text" placeholder="Digite o nome do Pokemon..." />
             <button onClick={() => buscarPokemon(pesquisa)}>🔎 Pesquisar</button>
-
             <hr />
 
             <div>
                 <h2>Nome: {pokemon.name}</h2>
                 <p>Tipo: {pokemon.types?.map(type => type.type.name).join(', ')}</p>
-                {/* <img src={pokemon.sprites.front_default} alt={pokemon.name} /> */}
+                <img src={pokemon.sprites?.other["official-artwork"].front_default} alt={pokemon.name} width="200" />
             </div>
         </div>
      );
